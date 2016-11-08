bf07158ac5b359f7e5b03e3ea394e95e2c6a9079
Ірина Попович < >
2016-11-08 21:31:53 +0200

    fixes from

--- app/src/main/java/com/example/bohdan/myorganizermain/MainActivity.java
+++ app/src/main/java/com/example/bohdan/myorganizermain/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.bohdan.myorganizermain;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,9 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.support.design.widget.NavigationView;
-
-import com.p_v.flexiblecalendar.FlexibleCalendarView;
 
 
 public class MainActivity extends AppCompatActivity {
